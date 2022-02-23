31392a46d6ee6729140bdc902f760de28c08bb97
Spencer Brazell <sa.brazell11@gmail.com>
2022-02-22 20:03:34 -0700

    Problem 5 working

--- makingDecisions2Lab.js
+++ makingDecisions2Lab.js
@@ -37,14 +37,18 @@ for (let i = 0; i < family.length; i++){
 
 // Do not edit the code below.
 var nums = [1,2,3,6,22,98,45,23,22,12];
-let evensArr = []
+let evensArr = [2]
 // Do not edit the code above.
 
 /*
   Use a for-loop to iterate through each number in the 'nums' array, if the number is even, append it to the 'evensArr'.
 */
 
-//Code Here
+for (let i = 0; i < nums.length; i++) {
+  if (nums[i] && 2 === 0) {
+    evensArr.push(nums[i])
+  }
+}
 
 
 
@@ -58,7 +62,17 @@ var score = 74
   Determine if the letter grade of the given variable 'score'. If the variable is a 90 or above, console log an 'A', between 80 and 89, console log a 'B', between 70 and 79, 'C', between 60 and 69, 'D', and anything below 60 should console log an 'F'.
 */
 
-//Code Here
+if (score >= 90){
+  console.log('A ')
+} else if(score >= 80){
+  console.log("B")
+} else if(score >= 70){
+  console.log("C")
+} else if(score >= 60){
+  console.log("D")
+} else if(score < 60){
+  console.log("F")
+}
 
 
 ////////// Intermediate Problems //////////
